7da90caa1624331af2ca972af422cd3c5d8fb5c7
arnasbastys <arnas.bastys@gmail.com>
2022-05-26 14:04:15 +0300

    Code formatting

--- tests/links.test.ts
+++ tests/links.test.ts
@@ -1,15 +1,13 @@
-import { test } from '@playwright/test';
-import { validateLinksInPage } from '../helpers/utilities'
+import { test } from "@playwright/test";
+import { validateLinksInPage } from "../helpers/utilities";
 
+test.describe("Parse links", () => {
+  test("Parse all links and check if they are < 400", async ({
+    page,
+    request,
+  }) => {
+    await page.goto("https://www.w3.org/standards/");
 
-test.describe('Parse links', () => {
-  test('Parse all links and check if they are < 400', async ({ page, request }) => {
-    await page.goto('https://www.w3.org/standards/');
-
-    await validateLinksInPage(page, request)
-
+    await validateLinksInPage(page, request);
   });
-
-
-
 });
